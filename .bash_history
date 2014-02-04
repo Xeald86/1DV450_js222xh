840ad6957ebe80bff244ef3d4afb54882929acf8
Jimmy Sigeklint <js222xh@student.lnu.se>
2014-02-04 19:14:58 +0000

    demo-test

---
+++ .bash_history
@@ -0,0 +1,7 @@
+git init
+git remote add origin https://github.com/Xeald86/Webbramverk.git
+git remote -v
+git add .
+git -m "Demo-commit"
+git commit -m "Demo-commit"
+git commit -m "Demo-commit"
---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '3717d848d3fe20a3f2dce68433244d4122a92c409e98e772ca47a7064c8790d60c53a435d9c2b35f6ec47a309b5ef51faee00a2ef90e80990adb528620ee3729'
