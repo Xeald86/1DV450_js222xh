d1f67bd3a0c0b37761a5530b8448ee3d465a6c90
Jimmy Sigeklint <js222xh@student.lnu.se>
2014-02-05 11:35:56 +0000

    ...

--- demo/app/controllers/say_controller.rb
+++ demo/app/controllers/say_controller.rb
@@ -2,8 +2,14 @@ class SayController < ApplicationController
   def hello
     @time = Time.now
     @files = Dir.glob('*')
+    @message = say_goodnight('Jimmy')
   end
 
   def goodbye
+  end
+  
+  def say_goodnight(name)
+    result = "Good night, #{name.capitalize}"
+    return result
   end
 end
